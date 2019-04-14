f25f2bbaf7bee070730f54058fb6cc3463da30c5
zouazano <sou.asano11@gmail.com>
2019-04-14 17:13:29 +0900

    follow relationships created

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,16 @@ class User < ApplicationRecord
 
   has_many :want_cups, dependent: :destroy
   has_many :wants_cups, through: :want_cups, source: :cup
+
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed
+
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+  has_many :followers, through: :passive_relationships, source: :follower
+
   
 end
