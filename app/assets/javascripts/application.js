c98c4f02ced1920f3bed8c9ba5b7dd47918903cf
zouazano <sou.asano11@gmail.com>
2019-05-05 08:37:13 +0900

    headers, footers made

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,11 @@
 //= require activestorage
 //= require_tree .
 //= require jquery3
+//= require jquery
+//= require jquery_ujs
 //= require popper
 //= require bootstrap-sprockets
+
+$(function () {
+  $('[data-toggle="popover"]').popover()
+})
