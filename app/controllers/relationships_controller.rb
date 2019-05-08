60b74dbd17d26cec3a8161ddab2d99b86f72b630
zouazano <sou.asano11@gmail.com>
2019-05-08 09:49:04 +0900

    userpage modifying began

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,7 +3,7 @@ class RelationshipsController < ApplicationController
     user=current_user
     @user=User.find(params[:user_id])
     if user.active_relationships.create(followed_id:@user.id)
-      redirect_to user_path(@user)
+      redirect_back(fallback_location: root_path)
       flash = 'フォローしました'
     else
       redirect_to root_url
@@ -14,7 +14,7 @@ class RelationshipsController < ApplicationController
     user=current_user
     @user=User.find(params[:user_id])
     if user.active_relationships.find_by(followed_id:@user.id).destroy
-      redirect_to user_path(@user)
+      redirect_back(fallback_location: root_path)
       flash = 'フォローを解除しました'
     else
       redirect_to root_url
