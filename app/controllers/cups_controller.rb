ccb911072bd29757245743fe3e1b9068e29c9131
zouazano <sou.asano11@gmail.com>
2019-05-11 19:28:45 +0900

    cups/show modified for followings

--- app/controllers/cups_controller.rb
+++ app/controllers/cups_controller.rb
@@ -4,6 +4,17 @@ class CupsController < ApplicationController
   end
   def show
   	@cup = Cup.find(params[:id])
-  	
+    if user_signed_in?
+      @related_user_cups = []
+      @cup.user_cups&.where(user_id:current_user.id).each do |hoga|
+        @related_user_cups << hoga
+      end
+      current_user.following&.each do |hoge|
+        @cup.user_cups&.where(user_id:hoge.id).each do |hoga|
+          @related_user_cups << hoga
+        end
+      end
+      @unrelated_user_cups = @cup.user_cups - @related_user_cups
+    end
   end
 end
