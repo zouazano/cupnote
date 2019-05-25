5b2d13567262e710621c356a769a4a748ac993bb
zouazano <sou.asano11@gmail.com>
2019-05-25 17:20:48 +0900

    like ajax torima

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,36 +18,3 @@
 //= require raty-js
 //= require popper
 //= require bootstrap-sprockets
-
-$(function () {
-  $('[data-toggle="popover"]').popover(), 
-  $('.raty-form').raty({
-    score: 0,
-    starType: 'i',
-    half: true,
-    numberMax: 5,
-    precision: true,
-    target: undefined,
-    targetKeep: true,
-  })
-  $('.raty-read').raty({
-    score: 3.5,
-    starType: 'i', 
-    half: true, 
-    readOnly: true,
-  })
-  var scroll = new SmoothScroll('a[href^="#"], [data-scroll]',{
-    offset: 50
-  });
-})
-
-$(function(){
-$('a[href^="#"]').click(function() {
-var speed = 400;
-var href= $(this).attr("href");
-var target = $(href == "#" || href == "" ? 'html' : href);
-var position = target.offset().top;
-$('body,html').animate({scrollTop:position}, speed, 'swing');
-return false;
-});
-});
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,20 +2,30 @@ class LikesController < ApplicationController
   def create
     user=current_user
     @user_cup=UserCup.find(params[:user_cup_id])
-    if Like.find_or_create_by(user_id: user.id, user_cup_id:@user_cup.id)
-      redirect_to timelines_index_path
-    else
-      redirect_to root_url
+    @like = Like.find_or_create_by(user_id: user.id, user_cup_id:@user_cup.id)
+    @like.reload
+    respond_to do |format|
+      format.html { redirect_to request.referrer || root_url }
+      format.js
     end
   end
 
   def destroy
     user=current_user
     @user_cup=UserCup.find(params[:user_cup_id])
-    if Like.where(user_id: user.id, user_cup_id: @user_cup.id).destroy_all
-      redirect_to timelines_index_path
-    else
-      redirect_to root_url
+    Like.where(user_id: user.id, user_cup_id: @user_cup.id).destroy_all
+    respond_to do |format|
+      format.html { redirect_to request.referrer || root_url }
+      format.js
     end
   end
+
+  private
+
+  def set_variables
+    @user_cup = UserCup.find(params[:user_cup_id])
+    @id_name = "#like-link-#{@user_cup.id}"
+    @id_like = "#like-#{@user_cup.id}"
+  end
+
 end
