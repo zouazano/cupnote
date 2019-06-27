bb51e2aa1ec5d68db7a7c4733f3ec5d366734861
zouazano <sou.asano11@gmail.com>
2019-06-27 16:21:07 +0900

    rank added

--- app/controllers/cups_controller.rb
+++ app/controllers/cups_controller.rb
@@ -4,6 +4,14 @@ class CupsController < ApplicationController
     @q = Cup.ransack(params[:q])
     @cups = @q.result(distinct: true)
     @makers = Maker.order('name ASC').all
+    @rank_cups = []
+    Cup.all.each do |cup|
+      unless cup.user_cups.first.present?
+        next
+      else
+        @rank_cups << cup
+      end
+    end
   end
 
   def show
